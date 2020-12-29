e620123b8e8cbd0f64eed2ed1d43653260ca8872
Yari Andres <yari.andres@gmail.com>
2020-12-29 13:14:27 +0100

    application js logic complted

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # javascrpit-todo
 
 * [x] Setup
-* [] create a header / hero section with a title
-* [] Create New Todo Form
-* [] Call a function when the form is submitted
-* [] Create a string property to store the user input
-  * [] Watch the property change as we type in the input
-* [] Log the user input when the form is submitted
-* [] Create an array property for todos
-* [] Push the new todo into the todos array, with done: false
-* [] Show the todos in a list
-* [] Check done on a todo to mark it as done
-  * [] Show a line through the todo tet
-* [] Add a button to delete a todo
+* [x] create a header / hero section with a title
+* [x] Create New Todo Form
+* [x] Call a function when the form is submitted
+* [x] Create a string property to store the user input
+* [x] Watch the property change as we type in the input
+* [x] Log the user input when the form is submitted
+* [x] Create an array property for todos
+* [x] Push the new todo into the todos array, with done: false
+* [x] Show the todos in a list
+* [x] Check done on a todo to mark it as done
+* [x] Show a line through the todo tet
+* [x] Add a button to delete a todo
 * [] Add a button to mark all todos as done
---
+++ app.js
@@ -0,0 +1,77 @@
+let todos = [
+	{
+		id: Math.floor(Math.random() * Math.floor(10000)),
+		name: 'Make dinner',
+		completed: false,
+	},
+	{
+		id: Math.floor(Math.random() * Math.floor(10000)),
+		name: 'Clean the house',
+		completed: false,
+	},
+	{
+		id: Math.floor(Math.random() * Math.floor(10000)),
+		name: 'Buy groceries',
+		completed: false,
+	},
+];
+
+// selectors
+const todoList = document.querySelector('.todoList');
+const todoItemName = document.querySelector('.todoList');
+const deleteAllbutton = document.querySelector('.deleteAllBtn');
+const todoElement = document.querySelector('.todoList');
+
+// Event listeners
+todoList.addEventListener('click', deletedTodo);
+todoItemName.addEventListener('click', markCompleted);
+deleteAllbutton.addEventListener('click', deleteAllTodos);
+
+
+function addTodo() {
+	let inputValue = document.getElementById('myText').value;
+
+	const todoObject = {
+		id: Math.floor(Math.random() * Math.floor(10000)),
+		name: inputValue,
+		completed: false,
+	};
+
+	todos.push(todoObject);
+	renderTodos();
+}
+
+function renderTodos() {
+	
+
+	todoElement.innerHTML = todos
+		.map(
+			todo => `            
+				<li class="todoItem">${todo.name}
+					<button class="deleteBtn">Delete</button>
+				</li>           
+            `,
+		)
+		.join('');
+}
+
+function markCompleted(e) {
+	const todoItem = e.target;
+
+	if (todoItem.classList[0] === 'todoItem') {
+		todoItem.classList.toggle('isCompleted');
+	}
+}
+
+function deletedTodo(e) {
+	const delBtn = e.target;
+
+	if (delBtn.classList[0] === 'deleteBtn') {
+		const todo = delBtn.parentElement;
+		todo.remove();
+	}
+}
+
+function deleteAllTodos(e) {
+	return todoElement.innerHTML = '';
+}
